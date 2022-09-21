5f1a28d780845b1d3f453324b114e30ff0ec33c6
Vijaythakur <44292527+Vijaynandthakur@users.noreply.github.com>
2022-09-21 23:35:29 +0530

    Add files via upload

---
+++ Vijumusicplayer/music-list.js
@@ -0,0 +1,144 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+    {
+      name: "Waqt Ki Baatien",
+      artist: "Dream Note",
+      img: "music",
+      href:"icon.png",
+      src: "music-2"
+    },
+    {
+      name: "mubarak-ho",
+      artist: "soham-naik-karanvir-sharma",
+      img: "music-2",
+      src: "mubarak-ho-soham-naik-karanvir-sharma-debattama-saha-meer-latest-hindi-romantic-songs-2022"
+    },
+    {
+      name: "teri-hogaiyaan",
+      artist: "vishal-mishra",
+      img: "music-2",
+      src: "teri-hogaiyaan-music-video-vishal-mishra-broken-but-beautiful-season-2-altbalaji"
+    },
+    {
+      name: "tera-zikr",
+      artist: "darshan-raval-official",
+      img: "music-4",
+      src: "tera-zikr-darshan-raval-official-video-latest-new-hit-song"
+    },
+    {
+      name: "Tera Hone Laga ",
+      artist: "Atif Aslam _ Alisha Chinai",
+      img: "music-6",
+      src:"music-6",
+      src: "Tera Hone Laga - Lyrical _ Atif Aslam _ Alisha Chinai _ Ajab Prem Ki Ghazab Kahani _ 2009"
+    },
+    {
+      name: "mehrama-love",
+      artist: "sara-pritam-darshan-raval",
+      img: "music-6",
+      src: "mehrama-love-aaj-kal-kartik-sara-pritam-darshan-raval-antara"
+    },
+    {
+      name: "Main Rang Sharbaton Ka",
+      artist: " Atif Aslam",
+      img: "music-6",
+      src: "Main Rang Sharbaton Ka - Phata Poster Nikhla Hero I Shahid,Ileana _ Atif Aslam "
+    },
+    {
+      name: "kali-kali-zulfon-ke",
+      artist: "madhur-sharma",
+      img: "music-6",
+      src: "kali-kali-zulfon-ke-madhur-sharma-ustad-nusrat-fateh-ali-khan-pearl-records"
+    },
+    {
+      name: "kal-ho-naa-ho",
+      artist: "sonu-nigam",
+      img: "music-6",
+      src: "kal-ho-naa-ho-full-video-title-track-shah-rukh-khan-saif-ali-preity-sonu-nigam-karan-j"
+    },
+    {
+      name: "fitoor",
+      artist: "arijit-singh-neeti-mohan",
+      img: "music-6",
+      src: "fitoor-song-shamshera-ranbir-kapoor-vaani-kapoor-arijit-singh-neeti-mohan-mithoon-karan-m"
+    },
+    {
+      name: "Chand Sifarish",
+      artist: "Kailash Kher",
+      img: "music-6",
+      src: "Chand Sifarish _ Full Song _ Fanaa _ Aamir Khan, Kajol _ Shaan, Kailash Kher _ Jatin-Lalit _ Prasoon"
+    },
+   
+    {
+      name: "junoon",
+      artist: "Lost",
+      img: "music-6",
+      src: "junoon "
+    },
+    {
+      name: "Top-20-best-ariana-grande-songs",
+      artist: "Arina Grande",
+      img: "music-6",
+      src: "top-20-best-ariana-grande-songs (1) "
+    },
+    {
+      name: "Top-papular-nagpuri-nonstop-songs-2021",
+      artist: "NCS Release",
+      img: "music-6",
+      src: "top-papular-nagpuri-nonstop-songs-2021-new-nagpuri-jukebox-nonstop-sadri-collection-2022 (1)"
+    },
+    {
+      name: "Best-of-emraan-hashmi-top-10-songs",
+      artist: "Emraan-hashmi",
+      img: "best-of-emraan-hashmi-top-10-songs-bollywood-hits-songs-2022-hindi-bollywood-romantic-songs (1)",
+    
+    },
+    {
+      name: "bhakti-bhajan-sachet-❤️-parampara-",
+      artist: "Sachet-❤️-Parampara",
+      img: "music-6",
+      src: "bhakti-bhajan-sachet-❤️-parampara-devotional-songs-nonstop-sachet-parampara-new-song (1)"
+    },
+    {
+      name: "Most-romantic-songs-♥️-hindi",
+      artist: "EverGreen",
+      img: "music-6",
+      src: "most-romantic-songs-♥️-hindi-love-songs-2020-latest-songs-2020-bollywood-new-song-indian-playlist (1)"
+    },
+    {
+      name: "Jubin-nautiyal-bhakti-songs",
+      artist: "Jubin-nautiyal",
+      img: "music-6",
+      src: "jubin-nautiyal-bhakti-songs-best-songs-of-jubin-nautiyal-bhajan-songs"
+    },
+    {
+      name: "श्री-हनुमान-चालीसा",
+      artist: "श्री-हनुमान-चालीसा",
+      img: "music-6",
+      src: "श्री-हनुमान-चालीसा-hanuman-chalisa-i-gulshan-kumar-i-hariharan-full-hd-video-shree-hanuman-chalisa"
+    },
+    {
+      name: "Mah Fav Song",
+      artist: "Subu",
+      img: "music-6",
+      src: "Top-trending-songs-2021-video-jukebox-latest-hindi-bollywood-tracks-2021-t-series"
+    },
+   
+    // like this paste it and remember to give comma after ending of this bracket }
+    // {
+    //   name: "Here is the music name",
+    //   artist: "Here is the artist name",
+    //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+    //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+    // }
+  ];
